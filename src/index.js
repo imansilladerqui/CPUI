cae282ddd36d3b6c93d6f104aa312a4bc8593929
imansilladerqui <imansilladerqui@hotmail.com>
2019-08-04 22:35:34 -0300

    routing dashboard

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import {createStore, applyMiddleware} from 'redux';
 import rootReducer from './store/reducers/rootReducer';
 import {Provider} from 'react-redux';
 import thunk from 'redux-thunk';
-
 import App from './App';
 
 
