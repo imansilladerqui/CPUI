12c03aaf2a83570b44898fa2c0b386b6e88feb8b
imansilladerqui <imansilladerqui@hotmail.com>
2020-01-28 17:05:17 -0300

    CP-146

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    // backendApi: 'https://protected-mountain-77919.herokuapp.com'
-    backendApi: 'http://localhost:8000'
+    backendApi: 'https://protected-mountain-77919.herokuapp.com'
+    // backendApi: 'http://localhost:8000'
 };
 
 
